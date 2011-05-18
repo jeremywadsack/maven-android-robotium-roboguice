88babecbc399696647a30664fd0932d19da4105e
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2011-05-17 17:00:53 -0700

    Removed robotium from the mix

--- test/src/main/java/com/example/android/tests/MainActivityTest.java
+++ test/src/main/java/com/example/android/tests/MainActivityTest.java
@@ -2,14 +2,13 @@ package com.example.android.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
-import com.jayway.android.robotium.solo.Solo;
 import com.example.android.MainActivity;
 import com.example.android.R;
 
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    private Solo solo;
+//    private Solo solo;
 
     public MainActivityTest() {
         super("com.example.android", MainActivity.class);
@@ -18,18 +17,18 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     @Override
     public void setUp() {
-        solo = new Solo(getInstrumentation(), getActivity());
+//        solo = new Solo(getInstrumentation(), getActivity());
     }
 
     @Override
     public void tearDown() throws Exception {
-        try {
-            //Robotium will finish all the activities that have been opened
-            //noinspection FinalizeCalledExplicitly
-            solo.finalize();
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
+//        try {
+//            //Robotium will finish all the activities that have been opened
+//            //noinspection FinalizeCalledExplicitly
+//            solo.finalize();
+//        } catch (Throwable e) {
+//            e.printStackTrace();
+//        }
         getActivity().finish();
         super.tearDown();
     }
@@ -37,13 +36,13 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     @SmallTest
     public void test_shouldHaveCorrectTitle() {
-        String appName = solo.getString(R.string.app_name);
+        String appName = getActivity().getString(R.string.app_name);
         assertEquals(appName, "Example");
     }
 
     @SmallTest
     public void test_shouldLaunchMainActivity() throws Exception {
-        solo.assertCurrentActivity("Current activity is not main.", MainActivity.class);
+//        solo.assertCurrentActivity("Current activity is not main.", MainActivity.class);
     }
 
 
